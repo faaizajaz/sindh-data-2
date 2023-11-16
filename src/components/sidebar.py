e78ffa7772240afaed6d1665746a1b825970e3ed
faaiz <faaizajaz@gmail.com>
2023-11-17 00:27:12 +0500

    Sort of works

--- src/components/sidebar.py
+++ src/components/sidebar.py
@@ -1,4 +1,5 @@
 import json
+from collections import deque
 
 import geopandas as gpd
 import pandas as pd
@@ -8,16 +9,18 @@ from dash.dcc.Store import Store
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
 from utils.settings import MAPBOX_TOKEN
+from werkzeug import datastructures
 
 
 def serve_sidebar():
     return html.Div(
         id="sidebar",
         children=[
+            dcc.Store(id="selected-parameters-memory"),
             html.H5("Select data"),
             dcc.Checklist(
                 id="data-selection",
-                options=["Schools", "Dispensaries", "SFERP Infra", "SFERP Livelihood"],
+                options=["SFERP Infra", "SFERP Livelihood", "Dispensaries", "Schools"],
             ),
             html.Div(id="test-data"),
         ],
@@ -26,27 +29,30 @@ def serve_sidebar():
 
 @callback(
     Output("main-map", "figure", allow_duplicate=True),
+    Output("selected-parameters-memory", "data"),
     Input("selected-districts-df", "data"),
+    Input("data-selection", "value"),
     prevent_initial_call=True,
 )
-def testthis(df):
-    if df is None:
+def sidebar_data_update(selected_districts_df, data_selection):
+    if selected_districts_df is None:
         raise PreventUpdate
-    x = pd.DataFrame.from_dict(df)
+    x = pd.DataFrame.from_dict(selected_districts_df)
 
-    x["new_bin"] = x.apply(_calc_binary, axis=1)
+    x["bin_combination"] = x.apply(_calc_binary, axis=1, args=(data_selection,))
 
-    x["new_bin"] = x["new_bin"].astype(str)
-    x["bin_parsed"] = x.apply(_parse_binary, axis=1)
+    x["bin_combination"] = x["bin_combination"].astype(str)
+    x["bin_combination_parsed"] = x.apply(_parse_binary, axis=1)
+    # print(data_selection)
 
-    print(x)
+    # print(x)
     fig = px.scatter_mapbox(
         x,
         lat=x.Latitude,
         lon=x.Longitude,
         # width=700,
         height=750,
-        color=x.bin_parsed,
+        color=x.bin_combination_parsed,
         color_discrete_sequence=px.colors.qualitative.Plotly,
     )
 
@@ -56,21 +62,30 @@ def testthis(df):
         margin=dict(l=30, r=30, t=30, b=30),
     )
 
-    print(x)
-    return fig
+    # print(x)
+    return fig, data_selection
 
 
-def _calc_binary(row):
+def _calc_binary(row, data_selection):
     bin_num = 0b0
+    print(data_selection)
 
-    if row["infra_uid"] is not None:
-        bin_num = bin_num + 0b1000
-    if row["scheme_uid"] is not None:
-        bin_num = bin_num + 0b0100
-    if row["disp_uid"] is not None:
-        bin_num = bin_num + 0b0010
-    if row["school_uid"] is not None:
-        bin_num = bin_num + 0b0001
+    if data_selection is None or len(data_selection) == 0:
+        return 0b0000
+
+    for selection in data_selection:
+        if selection == "SFERP Infra":
+            if row["infra_uid"] is not None:
+                bin_num = bin_num + 0b1000
+        if selection == "SFERP Livelihood":
+            if row["scheme_uid"] is not None:
+                bin_num = bin_num + 0b0100
+        if selection == "Dispensaries":
+            if row["disp_uid"] is not None:
+                bin_num = bin_num + 0b0010
+        if selection == "Schools":
+            if row["school_uid"] is not None:
+                bin_num = bin_num + 0b0001
 
     # return fixed length 4 bit number
     return f"{bin_num:04b}"
@@ -78,10 +93,11 @@ def _calc_binary(row):
 
 def _parse_binary(row):
     result = []
+
     outcomes = ["infra", "scheme", "disp", "school"]
 
-    for i in range(len(row["new_bin"])):
-        if row["new_bin"][i] == "1":
+    for i in range(len(row["bin_combination"])):
+        if row["bin_combination"][i] == "1":
             result.append(outcomes[i])
 
     if not result:
